b29a008cf0b858e7f8613c046c98b80c435d9138
Samuel <sam@infitialis.com>
2012-04-24 12:42:53 +0100

    Button fix

--- src/cloud9php/views/ajax/admin/users/new_user.php
+++ src/cloud9php/views/ajax/admin/users/new_user.php
@@ -1,52 +1,16 @@
-<div class="row">
-    <div class="span8">
-        <form id="auser">
-            <fieldset>
-                <legend><div align="center">Add New User</div></legend>
-                <div class="control-group">
-                    <label class="control-label" for="fname">First Name</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="fname" name="fname">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="lname">Last Name</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="lname" name="lname">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="email">Email Address</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="email" name="email">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="username">Username</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="username" name="username">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="password">Password</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="password" name="password">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="cpassword">Confirm Password</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="cpassword">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <button class="btn btn-primary" onClick="loadContentPost('/ajax/admin/users/create', $('#auser').serialize());">Create User</button>
-                        <button class="btn">Cancel</button>
-                    </div>
-                </div>
-            </fieldset>
+<div class="row" align="center">
+    <div class="span8" align="center">
+        <h1>Add New User</h1>
+        <form class="well" id="auser">
+            <input type="text" class="input-xlarge" placeholder="First Name...">
+            <input type="text" class="input-xlarge" placeholder="Last Name...">
+            <input type="text" class="input-xlarge" placeholder="Email Address...">
+            <input type="text" class="input-xlarge" placeholder="Username...">
+            <input type="text" class="input-xlarge" placeholder="Password...">
+            <input type="text" class="input-xlarge" placeholder="Confirm Password...">
         </form>
+        <button class="btn btn-primary" onClick="loadContentPost('/ajax/admin/users/create', $('#auser').serialize());">Create User</button>
+        <button class="btn">Cancel</button>
     </div>
     <div class="span4">
         <h2>Help</h2><br />
